8ec87a8f93b472ba6f0bed80ae40b7c3ba052eb4
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-03-30 14:20:04 +0700

    created addRecipe Mutation

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,24 @@
 export default {
   Query: {
     hello: () => "Hello",
-    getAllRecipes: () => {
-      return [];
+    getAllRecipes: (parent, args, { Recipe }) => {
+      return Recipe.find();
+    },
+  },
+  Mutation: {
+    addRecipe: async (parent, { data }, { Recipe }) => {
+      console.log(data);
+      const newRecipe = await new Recipe({
+        name: data.name,
+        category: data.category,
+        description: data.description,
+        instructions: data.instructions,
+        createdAt: data.createdAt,
+        likes: data.likes,
+        username: data.username,
+      }).save();
+
+      return newRecipe;
     },
   },
 };
